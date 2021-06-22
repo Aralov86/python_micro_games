af92d93a06793381a1e84dcea8e2836fe370a725
aralov91 <aralov199186@gmail.com>
2021-06-22 22:24:25 +0300

    Создал ежедневник, для записи задача/день

--- Projects/Diary.py
+++ Projects/Diary.py
@@ -7,12 +7,17 @@ task_2 = input("Введите задачу:")
 data_3 = input("Введите дату:")
 task_3 = input("Введите задачу:")
 
+data_4 = input("Введите дату:")
+task_4 = input("Введите задачу:")
+
 print(data, task)
 print(data_2, task_2)
 print(data_3, task_3)
+print(data_4, task_4)
 
 daily_planner = {
     data: task,
     data_2: task_2,
-    data_3: task_3
+    data_3: task_3,
+    data_4: task_4
 }
